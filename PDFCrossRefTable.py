4ce4c95b9ae641378ec3b3a835c69c69c80a27a4
Yu Liang <yu.liang@thekono.com>
2013-04-25 19:12:00 +0800

    init PDFCrossRefTable

---
+++ PDFCrossRefTable.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+# standard library import
+import logging as logger
+
+# third party related import
+
+# local library import
+
+
+class PDFCrossRefTable:
+
+	def __init__(self):
+
+		self.offset_dict = {}
+		self.free_dict = {}
+
